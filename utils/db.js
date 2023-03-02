7c7bcc26e6aba9f707fa9a3a894095d5915c0787
Pine Cone Coder <pinecone@kingdomstudios.io>
2023-03-02 10:57:58 +0100

    Maybe the double host is the reason why the database connection doesn't work

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const {Sequelize} = require('sequelize')
 
 /* Database */
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host   : process.env.DB_HOST,
+    // host   : process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
     logging: false,
     dialectOptions: {
