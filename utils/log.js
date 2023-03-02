c3a5ee717dcfc4a7b2c974ca782e410dac047c92
Gydo Makkinga <gydo@makkinga.online>
2023-03-02 21:39:49 +0100

    Uncommitted changes old repo

--- utils/log.js
+++ utils/log.js
@@ -8,7 +8,8 @@
 exports.error = function (interaction, code, error) {
     const reference = `${interaction.user.id.slice(-3)}-${interaction.channelId.slice(-3)}-${interaction.id.slice(-3)}`
 
-    console.error(`Error E${code.toString().padStart(3, '0')} (${reference}) by user ${interaction.user.id}:`, {
+    console.error({
+        'message'  : `Error E${code.toString().padStart(3, '0')} (${reference}) by user ${interaction.user.id}`,
         'user'     : interaction.user.id,
         'guild'    : interaction.guildId,
         'command'  : interaction.commandName,
