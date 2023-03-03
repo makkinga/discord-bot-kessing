73d30f1e7ec7b14e32352155a9dc73876c861f44
Gydo Makkinga <gydo@makkinga.online>
2023-03-03 21:46:02 +0100

    await needs to be in async of course

--- utils/db.js
+++ utils/db.js
@@ -18,17 +18,17 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     },
 })
 
-try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-} catch (error) {
-    console.error('Unable to connect to the database:', error)
-}
-
 /**
  * Sync database
  */
 exports.syncDatabase = async function () {
+    try {
+        await sequelize.authenticate()
+        console.log('Connection has been established successfully.')
+    } catch (error) {
+        console.error('Unable to connect to the database:', error)
+    }
+
     // await this.nonceCount.sync()
     // await this.accountHolders.sync()
     // await this.pendingGifts.sync()
