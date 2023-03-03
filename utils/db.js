9a33c63b48b1c54a18c92bd438dd41131a5d4bc5
Gydo Makkinga <gydo@makkinga.online>
2023-03-03 21:34:08 +0100

    Added db connection test

--- utils/db.js
+++ utils/db.js
@@ -16,6 +16,13 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     },
 })
 
+try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+} catch (error) {
+    console.error('Unable to connect to the database:', error)
+}
+
 /**
  * Sync database
  */
