f548300b3d42e3ce6d31fadd8e3eecb4c4738456
Gydo Makkinga <gydo@makkinga.online>
2023-03-03 21:28:34 +0100

    Making sure none of the code is executed

--- utils/db.js
+++ utils/db.js
@@ -37,83 +37,83 @@ exports.pineConeCounter = sequelize.define('pine_cone_count', {
     },
 }, {freezeTableName: true})
 
-/* Nonce counter */
-exports.nonceCount = sequelize.define('nonce', {
-    name : {
-        type     : Sequelize.STRING,
-        allowNull: false,
-        unique   : true
-    },
-    nonce: {
-        type     : Sequelize.INTEGER,
-        allowNull: false,
-    },
-}, {freezeTableName: true})
-
-/* Account holders */
-exports.accountHolders = sequelize.define('account_holder', {
-    user     : {
-        type     : Sequelize.STRING,
-        allowNull: false,
-    },
-    address  : {
-        type     : Sequelize.STRING,
-        allowNull: false,
-    },
-    role     : {
-        type   : Sequelize.BOOLEAN,
-        default: false
-    },
-    show_name: {
-        type   : Sequelize.BOOLEAN,
-        default: true
-    },
-    send_dm  : {
-        type   : Sequelize.BOOLEAN,
-        default: false
-    }
-})
-
-/* Pending gifts */
-exports.pendingGifts = sequelize.define('pending_gift', {
-    author: {
-        type     : Sequelize.STRING,
-        allowNull: false,
-    }
-})
-
-/* Gift cooldown */
-exports.giftCooldown = sequelize.define('gift_cooldown', {
-    user     : {
-        type     : Sequelize.STRING,
-        allowNull: false,
-    },
-    command  : {
-        type   : Sequelize.BOOLEAN,
-        default: false
-    },
-    claim    : {
-        type   : Sequelize.BOOLEAN,
-        default: false
-    },
-    timestamp: {
-        type     : Sequelize.INTEGER,
-        allowNull: false,
-    },
-})
-
-/* Message count */
-exports.messageCount = sequelize.define('message_count', {
-    user : {
-        type     : Sequelize.STRING,
-        allowNull: false,
-    },
-    guild: {
-        type     : Sequelize.STRING,
-        allowNull: false,
-    },
-    count: {
-        type     : Sequelize.INTEGER,
-        allowNull: false,
-    },
-}, {freezeTableName: true})
+// /* Nonce counter */
+// exports.nonceCount = sequelize.define('nonce', {
+//     name : {
+//         type     : Sequelize.STRING,
+//         allowNull: false,
+//         unique   : true
+//     },
+//     nonce: {
+//         type     : Sequelize.INTEGER,
+//         allowNull: false,
+//     },
+// }, {freezeTableName: true})
+//
+// /* Account holders */
+// exports.accountHolders = sequelize.define('account_holder', {
+//     user     : {
+//         type     : Sequelize.STRING,
+//         allowNull: false,
+//     },
+//     address  : {
+//         type     : Sequelize.STRING,
+//         allowNull: false,
+//     },
+//     role     : {
+//         type   : Sequelize.BOOLEAN,
+//         default: false
+//     },
+//     show_name: {
+//         type   : Sequelize.BOOLEAN,
+//         default: true
+//     },
+//     send_dm  : {
+//         type   : Sequelize.BOOLEAN,
+//         default: false
+//     }
+// })
+//
+// /* Pending gifts */
+// exports.pendingGifts = sequelize.define('pending_gift', {
+//     author: {
+//         type     : Sequelize.STRING,
+//         allowNull: false,
+//     }
+// })
+//
+// /* Gift cooldown */
+// exports.giftCooldown = sequelize.define('gift_cooldown', {
+//     user     : {
+//         type     : Sequelize.STRING,
+//         allowNull: false,
+//     },
+//     command  : {
+//         type   : Sequelize.BOOLEAN,
+//         default: false
+//     },
+//     claim    : {
+//         type   : Sequelize.BOOLEAN,
+//         default: false
+//     },
+//     timestamp: {
+//         type     : Sequelize.INTEGER,
+//         allowNull: false,
+//     },
+// })
+//
+// /* Message count */
+// exports.messageCount = sequelize.define('message_count', {
+//     user : {
+//         type     : Sequelize.STRING,
+//         allowNull: false,
+//     },
+//     guild: {
+//         type     : Sequelize.STRING,
+//         allowNull: false,
+//     },
+//     count: {
+//         type     : Sequelize.INTEGER,
+//         allowNull: false,
+//     },
+// }, {freezeTableName: true})
