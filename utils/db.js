70cd2eecc255f25fdd9f113ad32f60d4d99fac6c
Pine Cone Coder <pinecone@kingdomstudios.io>
2023-03-04 08:03:32 +0100

    Change to Postgres

--- utils/db.js
+++ utils/db.js
@@ -2,6 +2,7 @@ const {Sequelize} = require('sequelize')
 
 /* Database */
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host          : process.env.INSTANCE_UNIX_SOCKET,
     dialect       : process.env.DB_DIALECT,
     logging       : console.log,
     dialectOptions: {
