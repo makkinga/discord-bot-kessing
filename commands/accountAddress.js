bb96ccb046e85016313bea16f3b90fa4d1f68fbf
Gydo Makkinga <gydo@makkinga.online>
2023-03-22 10:02:48 +0100

    Added account address command

---
+++ commands/accountAddress.js
@@ -0,0 +1,32 @@
+const {SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder} = require('discord.js')
+const CryptoJS                                                             = require('crypto-js')
+const {Lang, Account, React}                                               = require('../utils')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('account-address')
+        .setDescription('Returns the wallet address connected to your tipping account'),
+
+    async execute(interaction)
+    {
+        // Defer reply
+        await interaction.deferReply({ephemeral: true})
+
+        const address = await Account.address(interaction.user.id)
+
+        // Checks
+        if (!address) {
+            return await React.error(interaction, Lang.trans(interaction, 'error.title.no_account'), Lang.trans(interaction, 'error.description.no_account'), {
+                edit  : true,
+                report: false,
+            })
+        }
+
+        // Send embed
+        const embed = new EmbedBuilder()
+            .setTitle(Lang.trans(interaction, 'account.title'))
+            .setDescription('```' + address + '```')
+
+        await interaction.editReply({embeds: [embed]})
+    },
+}
