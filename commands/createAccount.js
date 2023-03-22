972a03de501e4cb0ea32ace30ed40b0a94d5a77d
Gydo Makkinga <gydo@makkinga.online>
2023-03-22 10:02:15 +0100

    Added account check to create account

--- commands/createAccount.js
+++ commands/createAccount.js
@@ -1,6 +1,6 @@
 const {SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder} = require('discord.js')
 const CryptoJS                                                             = require('crypto-js')
-const {Lang}                                                               = require('../utils')
+const {Lang, Account, React}                                               = require('../utils')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,6 +12,15 @@ module.exports = {
         // Defer reply
         await interaction.deferReply({ephemeral: true})
 
+        // Checks
+        const address = await Account.address(interaction.user.id)
+        if (await Account.canTip(address)) {
+            return await React.error(interaction, Lang.trans(interaction, 'create.has_account.title'), Lang.trans(interaction, 'create.has_account.description', {username: `${interaction.user.username}#${interaction.user.discriminator}`, address: `${address.substr(0, 6)}...${address.substr(-4, 4)}`}), {
+                edit  : true,
+                report: false,
+            })
+        }
+
         // Get data
         const id = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(interaction.user.id), process.env.CREATE_ACCOUNT_CYPHER_SECRET).toString()
 
@@ -19,7 +28,6 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setTitle(Lang.trans(interaction, 'create.title'))
             .setDescription(Lang.trans(interaction, 'create.description'))
-
 
         const queryString = `?id=${id.replaceAll('+', ':p:').replaceAll('/', ':s:')}`
 
