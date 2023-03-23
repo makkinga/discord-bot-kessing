10310106647d3a8c32cb3d66d126aa0a03da0de6
Gydo Makkinga <gydo@makkinga.online>
2023-03-23 11:24:41 +0100

    Options not defined

--- utils/react.js
+++ utils/react.js
@@ -21,20 +21,20 @@ exports.error = async function (interaction, title, description = null, options 
         embed.setDescription(description)
     }
 
-    if (options.report) {
+    if (options.report ?? false) {
         embed.addFields(
             {name: Lang.trans(interaction, 'error.title.bug_report'), value: Lang.trans(interaction, 'error.description.bug_report', {server: 'https://discord.gg/2CUcKRzCUj'}), inline: false},
         )
     }
 
-    if (options.code) {
+    if (options.code ?? false) {
         embed.addFields(
             {name: `Error code`, value: `\`E${code.toString().padStart(3, '0')}\``, inline: true},
             {name: `Reference`, value: `\`${reference}\``, inline: true}
         )
     }
 
-    if (options.edit) {
+    if (options.edit ?? false) {
         await interaction.editReply({embeds: [embed], ephemeral: true})
     } else {
         await interaction.reply({embeds: [embed], ephemeral: true})
